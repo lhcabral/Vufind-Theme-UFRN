82bfef4bfd2a57eedd7095829dad8a1a0ded05a1
root <root@metabuscador.bczm.ufrn.br>
2023-04-03 13:46:52 +0000

    Arredondamento dos quantitativos na página inicial

--- js/ufrn.js
+++ js/ufrn.js
@@ -4,8 +4,8 @@ fetch("/api/v1/search?type=AllFields&facet%5b%5d=format&sort=relevance&page=1&li
     let totalMaster = result.facets.format.filter(el => el.value == 'masterThesis')
     let totalDoctoral = result.facets.format.filter(el => el.value == 'doctoralThesis')
     
-    document.getElementById('ufrn-total-count').innerHTML = result.resultCount.toLocaleString()
-    document.getElementById('ufrn-sigaa-count').innerHTML = totalSigaa[0].count.toLocaleString()
-    document.getElementById('ufrn-portal-count').innerHTML = totalPortal[0].count.toLocaleString()
-    document.getElementById('ufrn-repositorio-count').innerHTML = (totalMaster[0].count + totalDoctoral[0].count).toLocaleString()
+    document.getElementById('ufrn-total-count').innerHTML = (Math.floor(result.resultCount/1000)*1000).toLocaleString() + '+'
+    document.getElementById('ufrn-sigaa-count').innerHTML = (Math.floor(totalSigaa[0].count/1000)*1000).toLocaleString() + '+'
+    document.getElementById('ufrn-portal-count').innerHTML = (Math.floor(totalPortal[0].count/1000)*1000).toLocaleString() + '+'
+    document.getElementById('ufrn-repositorio-count').innerHTML = (Math.floor((totalMaster[0].count + totalDoctoral[0].count)/1000)*1000).toLocaleString() + '+'
 })
